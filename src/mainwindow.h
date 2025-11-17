f7e740548dc7d29a71db43211afe14ec0092eca3
Fujimakki <mikitko.batura@gmail.com>
2025-11-17 19:42:03 +0300

    fix: add src/ and gui/ dirs to cmake feat: add QSerial library

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSerialPort>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -19,5 +20,6 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QSerialPort *serPort;
 };
 #endif // MAINWINDOW_H
